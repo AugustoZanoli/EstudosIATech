21e340afe7526c5d07ecf86bd890ba990bac98d4
Zanoli <zancamar9@gmail.com>
2024-06-24 20:31:39 -0300

    "Instanciando o tokenizer"

--- beart.py
+++ beart.py
@@ -3,6 +3,8 @@ dados_treino = 'crepusculoDosIdolos.txt'
 
 from tokenizers import ByteLevelBPETokenizer
 
+# ---------------------------------- Treino do tokenizer ----------------------------------
+
 # Inicializando o tokenizer
 tokenizer = ByteLevelBPETokenizer()
 
@@ -23,3 +25,34 @@ print("ida: ")
 print(encodedString)
 print("volta: ")
 print(decodedString)
+
+# Salvando meu tokenizer em uma pasta
+tokenizer.save_model(PATH+'RAW_MODEL')
+# Ao analisar o arquivo vocab.json, vemos que ali está criado nosso vocabulario, nosso dicionário. Ali está todo o mapeamento dos nossos tokens.
+
+# Já o merges.txt são os pares que ele utilizou para treinar nosso tokenizer
+
+# ---------------------------------- Construindo o tokenizer ----------------------------------
+
+# Cria um tokenizer baseado no algoritmo BPE
+
+from tokenizers.implementations import ByteLevelBPETokenizer
+from tokenizers.processors import BertProcessing # Modelo bi-direcional
+
+# Instanciando meu modelo
+
+#tokenizer = ByteLevelBPETokenizer(
+#    PATH+'RAW_MODEL'+"vocab.json",
+#    PATH+'RAW_MODEL'+"merges.txt",
+#)
+#tokenizer._tokenizer.post_processor = BertProcessing(
+#    ("</s>", tokenizer.token_to_id("</s>")),
+#    ("<s>", tokenizer.token_to_id("<s>")),
+#)
+#tokenizer.enable_truncation(max_length=512)
+
+# Ao invés de usar o BERT, irei usar RobertaTokenizer
+
+from transformers import RobertaTokenizer
+
+tokenizer = RobertaTokenizer.from_pretrained(PATH+'RAW_MODEL', max_length=512)
